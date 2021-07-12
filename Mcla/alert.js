fc64ef9f9d08f69f9ade5da37fdd6f0f4494cfba
Andres C <69328357+andres-c94@users.noreply.github.com>
2021-07-12 16:46:28 -0500

    Add files via upload

---
+++ Mcla/alert.js
@@ -0,0 +1,54 @@
+
+function alerta(){
+    Swal.fire({
+
+        title: "Marcela!",
+        text: "No olvides que te Amo!",
+        confirmButtonText:"Chau!",
+        showConfirmButton: true,
+        confirmButtonColor:'#a52a2a',
+        imageWidth:'200px',
+        imageUrl:'cora.png'
+    
+        
+    });
+    
+}
+
+function alerta2(){
+
+    Swal.fire({
+
+        text: "Este no es!",
+        confirmButtonText:"cerrar",
+        //background:"#fa8fb1",
+        showConfirmButton: true,
+        confirmButtonColor:'#a52a2a',
+        imageWidth:'50px',
+        //imageUrl,
+        icon:'error'
+    
+        
+    });
+
+
+
+}
+
+function alerta3(){
+
+    Swal.fire({
+
+        title: "Este no es!",
+        text: "Pero te ganaste unos deliciosos chicharrones ;)",
+        confirmButtonText:"Chau!",
+        //background:"#fa8fb1",
+        showConfirmButton: true,
+        confirmButtonColor:'#a52a2a',
+        imageWidth:'200px',
+        imageUrl:'pig.png',
+    });
+
+
+
+}
